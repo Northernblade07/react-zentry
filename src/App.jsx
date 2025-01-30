0608d4591400da7ff025473314ce76112cb8e4fa
Prashant Bhandari <prashantbhandaricr07@gmail.com>
2025-01-30 11:50:37 +0530

    final commit

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,7 @@ import Features from './components/Features'
 import Footer from './components/Footer'
 import Hero from './components/Hero'
 import Navbar from './components/Navbar'
-import Story from './components/story'
+import Story from './components/Story'
 
 const App = () => {
   return (
